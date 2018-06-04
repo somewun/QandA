4beaa042d8853536ef450ca9cfddb1d7924fb8fc
somewun <somewuns@hotmail.com>
2018-06-04 15:31:09 +0100

    Update QandA.py

--- QandA.py
+++ QandA.py
@@ -126,7 +126,7 @@ ID,Question,Answer
 
 QandA, a simple revision quiz application. Version 0.1.0.
 Copyright (C) 2018  Somewun somewuns@hotmail.com
-For full source code see www.github.com/........
+For full source code see www.github.com/somewun/QandA.
  
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  
